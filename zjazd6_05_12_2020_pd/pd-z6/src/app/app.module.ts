30db248c83eb9458681c4d397e71ad931d7e7bb3
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2020-12-20 08:10:56 +0100

    dod rozwiazaniae task1 (nice guy at the gate)

--- zjazd6_05_12_2020_pd/pd-z6/src/app/app.module.ts
+++ zjazd6_05_12_2020_pd/pd-z6/src/app/app.module.ts
@@ -3,12 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {FormsModule} from '@angular/forms'; // do dzialania task1 (ngForms)
+
+// my components
+import {Task1Component} from './task1/task1.component'
+
 @NgModule({
   declarations: [
-    AppComponent
+      AppComponent,
+      Task1Component
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      FormsModule, 	// wymagane do dzialania task1 (ngForms)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ zjazd6_05_12_2020_pd/pd-z6/src/app/task1/task1.component.ts
@@ -0,0 +1,21 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'task1',
+    templateUrl: './task1.component.html'
+})
+export class Task1Component {
+    public nazwa: string = "hejka";
+    public fname: string = "";
+    public lname: string = "";
+    public greet() {
+	alert("witaj " + this.fname + " " + this.lname);
+    }
+    public bye() {
+	alert("zegnaj " + this.fname + " " + this.lname);
+	// additionally 
+	this.fname = "";
+	this.lname = "";
+    }
+}
+
