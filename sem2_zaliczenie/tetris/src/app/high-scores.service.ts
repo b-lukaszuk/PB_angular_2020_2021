836e40608f5b196ac93d17bc789ac94b89f3228f
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-06-19 11:58:53 +0200

    przenies. weryfikacji playerId z highScoresService do playerService

--- sem2_zaliczenie/tetris/src/app/high-scores.service.ts
+++ sem2_zaliczenie/tetris/src/app/high-scores.service.ts
@@ -2,7 +2,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IHighScore } from './highScore';
-import { IAuthToken } from './authToken'
 
 @Injectable({
     providedIn: 'root'
@@ -10,9 +9,8 @@ import { IAuthToken } from './authToken'
 export class HighScoresService {
 
     private _scoresUrl: string = "http://tetris.chrum.it/scores";
-    private _checkTokenUrl: string = "http://tetris.chrum.it/check-token";
     private _headersForScores: HttpHeaders = new HttpHeaders({
-        "accept": "application/json"
+        "Accept": "application/json",
     });
 
     constructor(private _http: HttpClient) { }
@@ -28,13 +26,4 @@ export class HighScoresService {
         this._http.post(this._scoresUrl, newScore,
             { headers: this._headersForScores });
     }
-
-    // prawidlowy token to 4 dowolne cyfry, np. "1234"
-    // do skonczenia, cos nie dziala tak jak trzeba
-    public isTokenCorrect(token: string): Observable<IAuthToken> {
-        console.log("checking token", token);
-        return this._http.post<IAuthToken>(this._checkTokenUrl,
-            { "auth-token": token });
-    }
-
 }
--- sem2_zaliczenie/tetris/src/app/player-data.service.ts
+++ sem2_zaliczenie/tetris/src/app/player-data.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IAuthToken } from './authToken'
 
 @Injectable({
     providedIn: 'root'
 })
 export class PlayerDataService {
 
-    constructor() { }
+    constructor(private _http: HttpClient) { }
+
+    private _checkTokenUrl: string = "http://tetris.chrum.it/check-token";
+    private _headersForTokenAuth: HttpHeaders = new HttpHeaders({
+        "Accept": "application/json",
+        'Content-Type': 'application/json',
+    });
     private _name: string = "a";
     private _playerId: string = "123";
 
@@ -19,5 +28,23 @@ export class PlayerDataService {
     public setPlayerData(name: string, id: string) {
         this._name = name;
         this._playerId = id;
+    }
+
+    // prawidlowy token to 4 dowolne cyfry, np. "1234"
+    // do skonczenia, cos nie dziala tak jak trzeba
+    public isTokenCorrect(token): Promise<void | boolean> {
+        console.log("checking token", token);
+        const request: Object = { "auth-token": token };
+        return this._http.post<IAuthToken>(this._checkTokenUrl,
+            request,
+            { headers: this._headersForTokenAuth })
+            .toPromise()
+            .then((data) => {
+                console.log("service", data["success"]);
+                return data["success"];
+            })
+            .catch((err) => {
+                console.log(err);
+            })
     }
 }
