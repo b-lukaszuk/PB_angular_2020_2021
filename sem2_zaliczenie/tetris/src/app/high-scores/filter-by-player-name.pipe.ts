1877d5d41319ee7e17a4cedd8c9fc0235d7553f0
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-06-20 13:19:03 +0200

    dodanie pipe-a filtrujacego high-score-y po playerName

---
+++ sem2_zaliczenie/tetris/src/app/high-scores/filter-by-player-name.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { IHighScore } from '../highScore';
+
+@Pipe({
+    name: 'filterByPlayerName'
+})
+export class FilterByPlayerNamePipe implements PipeTransform {
+
+    transform(highScores: IHighScore[], playerName: string,
+        active: boolean): IHighScore[] {
+        console.log("filtering by name", playerName);
+        if (!active) {
+            return highScores;
+        } else {
+            let result = highScores.filter((score) => {
+                console.log("testing name", score.name);
+                return score.name === playerName;
+            });
+            return result;
+        }
+    }
+
+}
